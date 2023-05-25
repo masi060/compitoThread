643610ac6087ad8a108f80f9ad80576be08e506d
lai2-01 <lai2-01@itismeucci.com>
2023-05-25 14:36:11 +0200

    bonus esercizio

--- src/semaforo.java
+++ src/semaforo.java
@@ -2,11 +2,11 @@ public class semaforo {
     private int valore;
 
     public semaforo(){
-        valore = 1;
+        valore = 2;
     }
 
     public synchronized void P(){
-        
+
         while(valore == 0){
             try{
                 System.out.println(Thread.currentThread().getName()+" e' in attesa");
